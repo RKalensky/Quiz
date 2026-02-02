d213d1d72088a192319aa187053ff633b2ab184f
roman kalenskyi <rkalenskyi@ccaeducate.me>
2026-02-02 17:49:31 +0200

    feat(game): refactoring

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -18,7 +18,7 @@ const Modal = ({
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-[40px] p-[40px] flex flex-col border-2 border-white bg-gray-800 z-50">
+    <div className="fixed inset-10 p-10 flex flex-col border-2 border-white bg-gray-800 z-50">
       <div className="mb-20">
         <h1 className="mb-0!">{title}</h1>
         {subTitle ? (
@@ -30,7 +30,7 @@ const Modal = ({
       <div className="flex-1 justify-center content-center text-center">
         {children}
       </div>
-      <button className="absolute top-[20px] right-[20px]" onClick={onClose}>
+      <button className="absolute top-5 right-5" onClick={onClose}>
         Close
       </button>
     </div>
